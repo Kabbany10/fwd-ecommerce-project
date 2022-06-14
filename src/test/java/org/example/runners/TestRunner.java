de6444c8c74a852489899b5dcf0d19938e488c4b
Mohamed El-Kabbany <mohkabany97@gmail.com>
2022-06-14 15:02:54 +0200

    Refactor

--- src/test/java/org/example/runners/TestRunner.java
+++ src/test/java/org/example/runners/TestRunner.java
@@ -3,7 +3,8 @@ package org.example.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features ="src/main/resources/features" , glue = "org/example/definitions", tags = "@smoke")
+@CucumberOptions(features ="src/main/resources/features" , glue = "org/example/definitions", tags = "@smoke" ,
+plugin = {"pretty" , "html:target/cucumber.json" , "json:target/cucumber.json" , "junit:target/cukes.xml" , "rerun:target/rerun.txt"})
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
